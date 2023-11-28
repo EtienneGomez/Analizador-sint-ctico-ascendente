fd1a1d9c1b77e3fc87e5ad599131a47aa3617898
EtienneGomez <88955045+EtienneGomez@users.noreply.github.com>
2023-11-27 18:46:52 -0600

    Create Token.py

---
+++ Token.py
@@ -0,0 +1,30 @@
+class TipoToken:
+    # Aquí defines los diferentes tipos de tokens. Ejemplos:
+    IDENTIFICADOR = "IDENTIFICADOR"
+    SELECT = "SELECT"
+    FROM = "FROM"
+    DISTINCT = "DISTINCT"
+    COMA = "COMA"
+    PUNTO = "PUNTO"
+    ASTERISCO = "ASTERISCO"
+    EOF = "EOF"
+
+
+class Token:
+    def __init__(self, tipo, lexema, posicion=0):
+        self.tipo = tipo
+        self.lexema = lexema
+        self.posicion = posicion
+
+    def __eq__(self, otro):
+        if not isinstance(otro, Token):
+            return False
+        return self.tipo == otro.tipo
+
+    def __str__(self):
+        return f"{self.tipo} {self.lexema} {self.posicion}"
+
+
+# Ejemplo de cómo crear un token y imprimirlo
+token = Token(TipoToken.SELECT, "select", 1)
+print(token)  # Imprimirá: SELECT select 1
