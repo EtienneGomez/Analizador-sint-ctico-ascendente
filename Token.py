8f66937ecd0fc2376faec19329f185217a24347d
VillalobosLobo <130194331+VillalobosLobo@users.noreply.github.com>
2023-11-28 06:44:48 -0600

    Update Token.py

--- Token.py
+++ Token.py
@@ -1,1 +1,15 @@
-#falta añgo
+from TipoToken import TipoToken
+
+class Token:
+    def __init__(self, tipo, lexema, posicion=0):
+        self.tipo = tipo
+        self.lexema = lexema
+        self.posicion = posicion
+
+    def __eq__(self, otro):
+        if not isinstance(otro, Token):
+            return False
+        return self.tipo == otro.tipo
+
+    def __str__(self):
+        return f"{self.tipo} {self.lexema} {self.posicion}"
